8aab2d7ffd2b4191da8aab5b7a41e18ceef58915
tomohikoseven <tomohikoseven0905@gmail.com>
2022-08-30 16:22:01 +0900

    No.28 modify.

--- javascript/28.implement-str-str.js
+++ javascript/28.implement-str-str.js
@@ -11,27 +11,22 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-  let matchFlg = false;
-  let idx = 0;
-  if (!needle) return 0;
+  if (!needle || haystack == needle) return 0;
   if (haystack.length < needle.length) return -1;
 
   for (let i = 0; i < haystack.length - needle.length + 1; i++) {
-    for (let j = 0; j < needle.length; j++) {
-      if (haystack[i + j] !== needle[j]) {
-        break;
-      } else {
-        if (j == needle.length - 1) {
-          matchFlg = true;
+    if (haystack[i] === needle[0]) {
+      for (let j = 0; j < needle.length; j++) {
+        if (haystack[i + j] !== needle[j]) {
+          break;
+        } else if (j == needle.length - 1) {
+          return i;
         }
       }
     }
-    if (matchFlg) {
-      idx = i;
-      break;
-    }
+
   }
-  return matchFlg ? idx : -1;
+  return -1;
 };
 exports.strStr = strStr;
 // @lc code=end
