9bfdfd09e76d4712a8d9af8c9d96eea39c8c538d
tomohikoseven <tomohikoseven0905@gmail.com>
2022-09-04 07:30:52 +0900

    No.35 second answer. It is OK.

--- javascript/35.search-insert-position.js
+++ javascript/35.search-insert-position.js
@@ -11,27 +11,30 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-
-  if (nums.length == 0) return 0;
-  if (nums[nums.length - 1] < target) return nums.length;
-  if (nums[0] >= target) return 0;
-
-  let left = 1;
-  let right = nums.length - 1;
-  let mid = Math.round((right + left) / 2);
-  while (left <= right) {
-    if (nums[mid] == target) return mid;
-    if (nums[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-    mid = Math.round((right + left) / 2);
-  }
-
-  return mid;
+  const left = 0;
+  const right = nums.length - 1;
+  return binSearch(nums, target, left, right);
 
 };
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+function binSearch(nums, target, left, right) {
+  if (left > right) return left;
+
+  const mid = Math.floor((left + right) / 2);
+  if (nums[mid] === target) return mid;
+  if (target < nums[mid]) {
+    return binSearch(nums, target, left, mid - 1);
+  }
+  else {
+    return binSearch(nums, target, mid + 1, right);
+  }
+}
 exports.searchInsert = searchInsert;
 // @lc code=end
 
